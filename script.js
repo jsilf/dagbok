26e1deb1ec5749c534cd3b5a888f12773658872a
Jessica <92667872+jsilf@users.noreply.github.com>
2021-12-01 17:14:22 +0100

    WIP working on local storage

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let textTitle = document.getElementById("textTitle");
 let textDate = document.getElementById("textDate");
 let textEntry = document.getElementById("textEntry");
 let saveBtn = document.getElementById("saveBtn");
-let diaryContent = document.getElementById("diaryContent");
+let entries = document.getElementById("entries");
 
 //funktion för att skapa dagboksinlägget vi fyllt i
 function getEntry(textTitle, textDate, textEntry) {
@@ -13,30 +13,24 @@ function getEntry(textTitle, textDate, textEntry) {
 
     //skapa variabel och ett element för titel
     let saveTitle = document.createElement("h4");
-    saveTitle.innerText = "Titel: " + textTitle.value;
+    saveTitle.innerText = textTitle.value;
     entry.append(saveTitle);
 
-    localStorage.setItem("title", saveTitle);
-    let lsTitle = localStorage.getItem("title");
-    console.log(lsTitle);
+    const storedTitle = localStorage.getItem("title");
+
+    if (textTitle) {
+        saveTitle.value = storedTitle;
+    }
 
     //skapa variabel och ett element för datumet
-    let saveDate = document.createElement("p");
-    saveDate.innerText = "Datum: " + textDate.value;
+    let saveDate = document.createElement("h5");
+    saveDate.innerText = textDate.value;
     entry.append(saveDate);
-
-    localStorage.setItem("date", saveDate);
-    let lsDate = localStorage.getItem("date");
-    console.log(lsDate);
 
     //skapa variabel och ett element för texten
     let saveEntry = document.createElement("p");
     saveEntry.innerText = textEntry.value;
     entry.append(saveEntry);
-
-    localStorage.setItem("text", saveEntry);
-    let lsEntry = localStorage.getItem("text");
-    console.log(lsEntry);
 
     document.body.append(entry);
 }
@@ -46,30 +40,15 @@ saveBtn.addEventListener("click", (evt) => {
     evt.preventDefault();
 
     getEntry(textTitle, textDate, textEntry);
-
-    // if (value) {
-    //     localStorage.setItem("namn", value);
-    //     location.reload();
-    // }
+    setLs();
 
     document.getElementById("textEntry").value = "";
+    document.getElementById("textDate").value = "";
     document.getElementById("textTitle").value = "";
 });
 
-// for (let i = 0; i < localStorage.length; i++) {
-
-//     const namn = localStorage.key(i);
-//     const value = localStorage.getItem(namn);
-
-//     //visa värdet jag skriver i input
-//     myName.innerHTML += `${value}`;
-
-//     //visa radera-knapp
-//     document.body.append(removeBtn);
-
-//     //ta bort värdet
-//     removeBtn.addEventListener("click", () => {
-//         removeBtn.remove();
-//         localStorage.removeItem(namn);
-//         myName.innerHTML = "";
-//     });
+const setLs = () => {
+    localStorage.setItem("title", textTitle.value);
+    localStorage.setItem("date", textDate.value);
+    localStorage.setItem("text", textEntry.value);
+};
